adc40b8a76dcd486649bc59000d7e2619464557b
Alex Karle <alex@alexkarle.com>
2022-11-14 14:42:57 -0500

    rsvp: Log to /tmp/ in dev mode to not pollute REPL

--- src/dev.scm
+++ src/dev.scm
@@ -1,8 +1,15 @@
 ;; dev.scm -- "dev mode" (runs main as thread for REPL development)
 (import srfi-18
+        spiffy
         (chicken process-context))
 
 (include "main.scm")
+
+(access-log "/tmp/rsvp-access.log")
+(error-log "/tmp/rsvp-error.log")
+
+(print "Logging access to " (access-log))
+(print "Logging error to " (error-log))
 
 (define thread
   (begin
